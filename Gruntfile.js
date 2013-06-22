0d11f55d41824571bb29c7933a0c59951a886790
Timothy Guan-tin Chien <timdream@gmail.com>
2013-06-22 13:14:30 +0800

    grunt build script for production

---
+++ Gruntfile.js
@@ -0,0 +1,51 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      prodution: 'production'
+    },
+    copy: {
+      'htaccess': { expand: true, src: '.htaccess', dest: 'production'},
+      'root': { expand: true, src: '*', dest: 'production', filter: 'isFile' },
+      'locales': { expand: true, src: 'locales/*', dest: 'production' },
+      'css': { expand: true, src: 'assets/*.css',
+               dest: 'production', filter: 'isFile' },
+      'images': { expand: true, src: 'assets/images/*', dest: 'production' },
+      'canvas-to-blob': {
+        expand: true,
+        src: 'assets/canvas-to-blob/canvas-to-blob.min.js',
+        dest: 'production' }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> ' +
+                ' <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      }
+    },
+    useminPrepare: {
+      html: 'index.html',
+      options: {
+        dest: 'production'
+      }
+    },
+    usemin: {
+      html: 'production/index.html',
+      options: {
+        dirs: ['assets']
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-useMin');
+
+  // Default task(s).
+  grunt.registerTask('default',
+      ['clean', 'copy', 'useminPrepare', 'concat', 'uglify', 'usemin']);
+};
