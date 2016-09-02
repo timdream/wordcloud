445068fe20f0ab8b074503deb5eb47e04c2e3969
Timothy Guan-tin Chien <timdream@gmail.com>
2016-09-02 17:45:46 +0800

    Updates

--- assets/downloader-worker.js
+++ assets/downloader-worker.js
@@ -1,2 +1,2 @@
-/*! wordcloud-app  2016-06-02 */
+/*! wordcloud-app  2016-09-02 */
 "use strict";self.callback=function(){var a=Array.prototype.slice.call(arguments);self.postMessage(a)},self.onmessage=function(a){var b=a.data;b+=-1===b.indexOf("?")?"?":"&",b+="callback=callback",self.importScripts(b)};
