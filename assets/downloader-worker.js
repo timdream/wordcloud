3da58ea61be40880255f12a465ee2ca78c9ac424
Timothy Guan-tin Chien <timdream@gmail.com>
2021-01-30 18:58:16 -0800

    Updates

--- assets/downloader-worker.js
+++ assets/downloader-worker.js
@@ -1,3 +1,3 @@
-/*! wordcloud-app  2020-06-06 */
+/*! wordcloud-app  2021-01-30 */
 
 "use strict";self.callback=function(){var a=Array.prototype.slice.call(arguments);self.postMessage(a)},self.onmessage=function(a){var l=a.data;l+=-1===l.indexOf("?")?"?":"&",l+="callback=callback",self.importScripts(l)};
